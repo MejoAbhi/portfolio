a2f55dd8012d51dba7d7c60d6fdfd0cb34943d0b
AbhiMejo <abhishekgithub6@gmail.com>
2024-08-01 02:23:43 +0530

    commit service

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Service from './components/Service'
 import Expretise from './components/Expretise'
 import Footer from './components/Footer'
 import { Route, Routes } from 'react-router-dom'
+import WebServices from './components/WebServices'
 //import MainPage from './components/MainPage'
 //import Header from './components/Header'
 
@@ -33,3 +34,57 @@ function App() {
 }
 
 export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// <div id="hs-toggle-between-modals-first-modal" classNameName="hs-overlay hidden size-full fixed top-0 start-0 z-[80] overflow-x-hidden overflow-y-auto pointer-events-none" role="dialog" tabindex="-1" aria-labelledby="hs-toggle-between-modals-first-modal-label">
+    // <div classNameName="hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-500 mt-0 opacity-0 ease-out transition-all sm:max-w-lg sm:w-full m-3 sm:mx-auto">
+    //     <div classNameName="flex flex-col bg-white border shadow-sm rounded-xl pointer-events-auto">
+    //     <div classNameName="flex justify-between items-center py-3 px-4 border-b">
+    //         <h3 id="hs-toggle-between-modals-first-modal-label" classNameName="font-bold text-gray-800">
+    //         Movie Management System
+    //         </h3>
+    //         <button type="button" classNameName="size-8 inline-flex justify-center items-center gap-x-2 rounded-full border border-transparent bg-gray-100 text-gray-800 hover:bg-gray-200 focus:outline-none focus:bg-gray-200 disabled:opacity-50 disabled:pointer-events-none" aria-label="Close" data-hs-overlay="#hs-toggle-between-modals-first-modal" data-hs-overlay-close="">
+    //         <span classNameName="sr-only">Close</span>
+    //         {/* <svg classNameName="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+    //             <path d="M18 6 6 18"></path>
+    //             <path d="m6 6 12 12"></path>
+    //         </svg> */}
+    //         </button>
+    //     </div>
+    //     <div classNameName="p-4 overflow-y-auto">
+    //         <p classNameName="mt-1 text-gray-800">
+    //         Show a second modal and hide this one with the button below.
+    //         </p>
+    //     </div>
+    //     </div>
+    //  </div>
+    // </div>
+    
--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react'
+import WebServices from './WebServices'
 
-const Service = (serviceRef) => {
+const Service = () => {
+    const [WebServicesModal,setWebServiceModal]=useState(false)
+    const [modalType,setModalType]=useState()
+    console.log("WebServicesModal",WebServicesModal)
     //for scrolling
     
 
@@ -8,7 +12,7 @@ const Service = (serviceRef) => {
     const[data1,setData1]=useState({
         id:"id",
         title:"Web Development",
-        description:`Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius a debitis, qui at ea aut illum quam? Voluptate, excepturi autem.`
+        description:`Welcome to my full-stack project portfolio! Here, you'll find a collection of web applications that showcase my skills in creating both beautiful and functional websites. Each project uses a mix of Spring Boot, Java, and ReactJS for the back end, along with Tailwind CSS and Bootstrap for the design. From online stores to social media tools,I blend front-end design with back-end technology to build user-friendly and efficient applications. Enjoy browsing through these examples of how I turn ideas into effective and engaging web solutions.`
     }
 )
     const[data2,setData2]=useState({
@@ -21,39 +25,46 @@ const Service = (serviceRef) => {
     const[data3,setData3]=useState({
         id:"3",
         title:"Backend Development",
-        description:`Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius a debitis, qui at ea aut illum quam? Voluptate, excepturi autem.`
+        description:`Welcome to my full-stack project portfolio! Here, you'll find a collection of web applications that showcase my skills in creating both beautiful and functional websites. Each project uses a mix of Spring Boot, Java, and ReactJS for the back end, along with Tailwind CSS and Bootstrap for the design. From online stores to social media tools,I blend front-end design with back-end technology to build user-friendly and efficient applications. Enjoy browsing through these examples of how I turn ideas into effective and engaging web solutions.`
     }
 )
+    const handelModalOpen=(test)=>{
+        setModalType(test)
+        setWebServiceModal(true)
+    }
   return (
+    <>
     <div id='serviceId'  className='main py-16 md:py-16 shadow-xl'>
         <h1 className='text-5xl text-bold underline text-center'>My Services</h1>
-        <div className='service-container mt-12 flex flex-col md:flex-row space-y-6  px-10 space-x-6'>
+        <div className='service-container mt-12 flex  flex-col md:flex-row space-y-6  px-10 space-x-6'>
             <div className='service1 mt-6 ml-6 bg-slate-100 hover:bg-gray-300 p-5 text-center shadow-lg rounded-xl space-y-4  '>
             <i class="text-4xl fa-brands fa-aws"></i>
-            <h1 className='text-4xl'>{data1.title}</h1>
+            <h1 className='text-3xl md:text-4xl'>{data1.title}</h1>
             <p>{data1.description}</p>
-            <button className='px-3 py-2 bg-gradient-to-r from-fuchsia-300 to-purple-500 hover:from-pink-500 hover:to-purple-300 rounded-full text-1xl'>Check</button>
+            {/* <a href={WebServices} className='px-3 py-2 bg-gradient-to-r from-fuchsia-300 to-purple-500 hover:from-pink-500 hover:to-purple-300 rounded-full text-1xl'>Check</a> */}
+
+            {/* <button onClick={()=>handelModalOpen("service")} type="button" class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none" aria-haspopup="dialog" aria-expanded="false" aria-controls="hs-toggle-between-modals-first-modal" data-hs-overlay="#hs-toggle-between-modals-first-modal">Open modal</button> */}
+
+            <button className='px-3 py-2 bg-gradient-to-r from-fuchsia-300 to-purple-500 hover:from-pink-500 hover:to-purple-300 rounded-full text-1xl' onClick={()=>handelModalOpen("service")}>Check</button>
+            
             </div>
 
 
             <div className='bg-slate-100 p-5 mt-6 ml-6 hover:bg-gray-300 text-center service2 shadow-lg rounded-xl space-y-4'>
            
             <i class="text-4xl fa-brands fa-java"></i>
-            <h1 className='text-4xl'>{data2.title}</h1>
-            <p>{data2.description}</p>
-            <button className='px-3 py-2 bg-gradient-to-r from-fuchsia-300 to-purple-500 hover:from-pink-500 hover:to-purple-300 rounded-full text-1xl'>Check</button>
-            </div>
-
-
-            <div className='bg-slate-100 p-5 mt-6 ml-6 hover:bg-gray-300 text-center service3 shadow-lg rounded-xl space-y-4'>
-            <i class="text-4xl fa-brands fa-android"></i>
             <h1 className='text-4xl'>{data3.title}</h1>
             <p>{data3.description}</p>
-            <button className='px-3 py-2 bg-gradient-to-r from-fuchsia-300 to-purple-500 hover:from-pink-500 hover:to-purple-300 rounded-full text-1xl'>Check</button>
+            <button className='px-3 py-2 bg-gradient-to-r from-fuchsia-300 to-purple-500 hover:from-pink-500 hover:to-purple-300 rounded-full text-1xl' onClick={()=>handelModalOpen("backendService")}>Check</button>
             </div>
+
+
+            
             
         </div>
     </div>
+    {WebServicesModal && <WebServices WebServicesModal={WebServicesModal} setWebServiceModal={setWebServiceModal} modalType={modalType}/>}
+    </>
   )
 }
 
