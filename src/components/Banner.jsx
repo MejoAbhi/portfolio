0193917d81d83021c30af27f20b637bdc0921072
MejoAbhi <abhishekgithub6@gmail.com>
2024-10-02 17:01:35 +0530

    version change

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useRef } from 'react'
 import bannerImage from '../assets/bg1.png';
 import bannnerBackground from '../assets/final.png';
 import Typed from 'typed.js';
+import FontAwesome from 'react-fontawesome'
+
 const Banner = () => {
 
   const el = useRef(null);
@@ -26,16 +28,16 @@ const Banner = () => {
     <div   className='main flex flex-col md:flex-row items-center h-full '>
        {/* this is text portion */}
         <div 
-        className=' w-full py-6 flex justify-center bg-gradient-to-r from-indigo-500 via-purple-300 to-white'>
-          <div className='w-2/3 space-y-2 ms-10'>
+        className=' w-full  py-6 flex justify-center bg-gradient-to-r from-indigo-500 via-purple-300 to-white'>
+          <div className='w-2/3 space-y-2 '>
           <h3 className='text-xl md:text-2xl lg:text-3xl font-semibold'>Hi, I am</h3>
           <h1 className='text-xl md:text-2xl lg:text-5xl font-bold'>Abhishek Singha</h1>
-          <h4 className='text-2xl md:text-3xl lg:text-4xl'>I am a <span className="" ref={el}></span></h4>
+          <h4 className='font-bold md:text-3xl lg:text-4xl'>I am a <span className="" ref={el}></span></h4>
           <p className='text-1xl'>Experienced in leveraging cutting-edge technologies to meet and exceed project goals</p>
       
        <div className='iconContainer space-x-2'>
        <a className='' href='https://github.com/MejoAbhi'><i class="fa-brands fa-github"></i></a>
-      <a className=''href='https://www.linkedin.com/in/abhishek-singha-65788520a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app'><i class="fa-brands text-4xl fa-linkedin"></i></a> 
+      <a className=''href='https://www.linkedin.com/in/abhishek-singha-65788520a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app'><FontAwesomeIcon icon="fa-brands fa-linkedin" /></a> 
        </div>
        
         <br/>
